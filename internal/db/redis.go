64cb1dadf2be23f74cb6fc3ed6d6c49508abc486
albanog00 <albanog00@gmail.com>
2023-11-18 19:10:49 +0100

    feat: added redis

---
+++ internal/db/redis.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"errors"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	Client *redis.Client
+}
+
+var (
+	ErrNil = errors.New("no matching record found in redis database")
+	Ctx    = context.TODO()
+)
+
+func NewRedisClient() (*RedisClient, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     ":6379",
+		Password: "",
+		DB:       0,
+	})
+
+	if err := client.Ping(Ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return &RedisClient{
+		Client: client,
+	}, nil
+}
--- internal/handlers/search.go
+++ internal/handlers/search.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+	"giuseppealbano.dev/img-searcher/internal/db"
 )
 
 type SearchResult struct {
@@ -20,27 +25,54 @@ func SearchHandler(c *fiber.Ctx) error {
 
 	// TODO: Search images
 
-	// Simulating images search
-	var result SearchResult
-	num := rand.Intn(100)
-	for i := 0; i < 12; i++ {
-		// generate random numbers
-		result.Urls = append(
-			result.Urls,
-			fmt.Sprintf("https://source.unsplash.com/random/?sig=%d", (i%5)*num),
-		)
+	redisClient, err := db.NewRedisClient()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Error",
+			"status":  fiber.StatusInternalServerError,
+			"result":  nil,
+		})
 	}
 
-	// json, err := json.Marshal(result)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	c.Status(fiber.StatusBadRequest)
-	// 	return c.JSON(fiber.Map{
-	// 		"message": "Failed to encode result",
-	// 		"status":  fiber.StatusBadRequest,
-	// 		"result":  nil,
-	// 	})
-	// }
+	var result SearchResult
+	res, err := redisClient.Client.Get(db.Ctx, q).Result()
+	if err == redis.Nil {
+		log.Printf("Key %s not found in Redis, searching...\n", q)
+
+		// Simulating images search
+		if len(result.Urls) == 0 {
+			num := rand.Intn(100)
+			for i := 0; i < 12; i++ {
+				// generate random urls
+				result.Urls = append(
+					result.Urls,
+					fmt.Sprintf("https://source.unsplash.com/random/?sig=%d", (i%5)*num),
+				)
+			}
+		}
+
+		// Cache result in redis
+		json, err := json.Marshal(result)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Internal Error",
+				"status":  fiber.StatusInternalServerError,
+				"result":  nil,
+			})
+		}
+		redisClient.Client.Set(db.Ctx, q, json, time.Hour*24)
+	} else {
+		log.Printf("Key %s found in Redis\n", q)
+
+		err = json.Unmarshal([]byte(res), &result)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Internal Error",
+				"status":  fiber.StatusInternalServerError,
+				"result":  nil,
+			})
+		}
+	}
 
 	// return result as json
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
